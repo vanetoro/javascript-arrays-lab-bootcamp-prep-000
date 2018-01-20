6ee886d1472ddea03e996e9ef6e0afb59ddb846d
Vanessa Toro <vanetoro84@gmail.com>
2018-01-20 16:45:30 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,6 +3,6 @@ const app = "I don't do much."
 kittens = ['Milo', 'Otis', 'Garfield'];
 
 function destructivelyAppendKitten(name){
-  kitten.push(name);
-  return kitten;
+  kittens.push(name);
+  return kittens;
 }
